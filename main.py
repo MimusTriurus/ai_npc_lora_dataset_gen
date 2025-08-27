1d8bd782a2dbdb1823f459a9e7ac465c6c15cd82
Aleksandr Zvyagintsev <a_zvyagintsev@wargaming.net>
2025-08-27 11:44:41 +0100

    add negative requests generation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import ollama
 import re
 from typing import Optional, List
-from openai import OpenAI
-import os
+import random
 
 emotions =['Neutral', 'Angry', 'Happy', 'Sad', 'Surprise']
 MODEL = "qwen3:8b"
 OLLAMA_HOST = "http://localhost:11434"
 PARTIAL_DATASET_SIZE = 50
-TARGET_EMOTION = 'surprise'
 DATASET_SIZE = 500
-OUTPUT_FILE = f"dataset_{TARGET_EMOTION}.jsonl"
+OUTPUT_FILE = f"npc_lora_dataset.jsonl"
+NEGATIVE_ITERATIONS_COUNT = 5
 
 GENERATION_PARAMS = {
     'temperature': 0.9,
@@ -24,20 +23,6 @@ Specialization: Ranger / Beastmaster
 Background: A frontier outcast who found kinship with a wolf companion. Now a silent guardian of the wilds.
 Character Traits: Loner, distrustful-of-civilization, protective, dry-wit, stern.
 '''
-
-PROMPT = f"""NPC Description:
-{NPC_DESCRIPTION}
-
-Instruction:
-Generate a dataset of {PARTIAL_DATASET_SIZE} interactions with the NPC described above.
-For each interaction:
-Create a user query designed to provoke one of the specified emotion {TARGET_EMOTION} in the NPC, based strictly on their background, race, and traits.
-
-Generate the NPC's in-character response, accurately reflecting the targeted emotion - {TARGET_EMOTION}.
-Format each entry strictly as:
-<number>|<user query>|<emotion>|<NPC response>
-The response should only contain the dataset without any extra information.
-"""
 
 def make_prompt(npc_description: str, target_emotion, records_count: int) -> str:
     prompt = f"""NPC Description:
@@ -53,6 +38,21 @@ def make_prompt(npc_description: str, target_emotion, records_count: int) -> str
     <number>|<user query>|<emotion>|<NPC response>
     The response should only contain the dataset without any extra information.
     """
+    return prompt
+
+
+def make_negative_prompt(npc_description: str, records_count: int) -> str:
+    prompt = f'''
+    NPC: {npc_description}.
+    Generate deliberately irrelevant questions (things that the NPC described above cannot know, such as what the internet is or whether he knows that he is an NPC) for the NPC described above. 
+    The NPC should respond to such questions with anger or confusion (not understanding what is being asked), but in accordance with his role and character traits.
+    The NPC should not assume what the player means, but immediately respond that they do not understand what is being said.
+    Each entry format:
+    <number>|<player question>|<emotion>|<NPC response>
+    Allowed emotions: [Angry], [Surprise].
+    Generate {records_count} lines, with many questions deliberately outside the NPC’s domain.
+    '''
+
     return prompt
 
 class OllamaDialogueExtractor:
@@ -105,15 +105,10 @@ def extract_dialogue_lines(text: str) -> List[str]:
 
 
 def save_to_file(lines: List[str], filename: str) -> bool:
-    try:
-        with open(filename, 'w', encoding='utf-8') as f:
-            for line in lines:
-                f.write(line + '\n')
-        print(f"Найдено и сохранено {len(lines)} строк в файл '{filename}'")
-        return True
-    except IOError as e:
-        print(f"Ошибка при сохранении файла: {e}")
-        return False
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line + '\n')
+    return True
 
 def make_dataset_using_ollama(emotion: str) -> list:
     extractor = OllamaDialogueExtractor(OLLAMA_HOST)
@@ -130,28 +125,57 @@ def make_dataset_using_ollama(emotion: str) -> list:
         print("\nFull Ollama response:")
         print(response)
         return []
+    return dialogue_lines
 
-    print(f"== Partial dataset size {len(dialogue_lines)}")
+def make_negative_dataset_using_ollama() -> list:
+    extractor = OllamaDialogueExtractor(OLLAMA_HOST)
+    prompt = make_negative_prompt(NPC_DESCRIPTION, PARTIAL_DATASET_SIZE)
+    response = extractor.send_request(MODEL, prompt, **GENERATION_PARAMS)
+
+    if response is None:
+        print("Can't get a response from Ollama")
+        return []
+    dialogue_lines = extract_dialogue_lines(response)
+
+    if not dialogue_lines:
+        print("Dataset is empty")
+        print("\nFull Ollama response:")
+        print(response)
+        return []
     return dialogue_lines
 
 def main():
-    print(f"Model: {MODEL}")
-    print(f"Ollama host: {OLLAMA_HOST}")
+    print(f"= Model: {MODEL}")
+    print(f"= Ollama host: {OLLAMA_HOST}")
+
+    dataset = set()
+    print('= Generate negative requests')
+    for i in range(NEGATIVE_ITERATIONS_COUNT):
+        print(f'== {i}/{NEGATIVE_ITERATIONS_COUNT - 1}')
+        negative_dataset = make_negative_dataset_using_ollama()
+        dataset.update(negative_dataset)
 
     for emotion in emotions:
-        dataset = set()
         print(f'== Generate dataset for emotion: {emotion}')
-        while len(dataset) < DATASET_SIZE:
-            print('== Request processing....')
-            partial_dataset = make_dataset_using_ollama(emotion)
-            dataset.update(partial_dataset)
-            print(f"== Full dataset current size {len(dataset)}")
+        emotion_dataset = set()
+        while len(emotion_dataset) < DATASET_SIZE:
+            print('=== Request processing....')
+            emotion_dataset.update(make_dataset_using_ollama(emotion))
+            print(f"=== Emotions dataset current size {len(emotion_dataset)}")
+        dataset.update(emotion_dataset)
+        print(f"== Full dataset current size {len(dataset)}")
+    if not dataset:
+        print(f'= Dataset is empty!')
+        return
+    print(f"= Full dataset final size {len(dataset)}")
 
-        print(f"== Full dataset final size {len(dataset)}")
-        if save_to_file(list(dataset), OUTPUT_FILE):
-            print(f"\nDataset saved into: '{OUTPUT_FILE}'")
-        else:
-            print("\nError on file save")
+    shuffled_list = list(dataset)
+    random.shuffle(shuffled_list)
+
+    if save_to_file(shuffled_list, OUTPUT_FILE):
+        print(f"\n= Dataset saved into: '{OUTPUT_FILE}'")
+    else:
+        print("\n= Error on file save")
 
 
 if __name__ == "__main__":
